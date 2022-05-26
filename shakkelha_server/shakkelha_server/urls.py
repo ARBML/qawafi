3f1aa5c6756cd86ff5ab2444913f369b6b1578e9
MagedSaeed <msmj2015@gmail.com>
2022-05-26 22:11:55 +0300

    push servers, remove unused one

--- shakkelha_server/shakkelha_server/urls.py
+++ shakkelha_server/shakkelha_server/urls.py
@@ -1,7 +1,7 @@
-"""bohour_server URL Configuration
+"""shakkelha_server URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import shakkelha_api
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/", include("shakkelha_api.urls")),
 ]
