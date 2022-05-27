901f60e545e5789bdc8108804b0bc4a19a2fc0eb
MagedSaeed <msmj2015@gmail.com>
2022-05-27 19:13:28 +0300

    move bohour to qawafi_server

--- qawafi_server/qawafi_server/settings.py
+++ qawafi_server/qawafi_server/settings.py
@@ -15,10 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-import sys
+#import sys
 
 # add base dir parent to path to include bohour package
-sys.path.append(f"{BASE_DIR}/..")
+#sys.path.append(f"{BASE_DIR}/..")
 
 
 # Quick-start development settings - unsuitable for production
