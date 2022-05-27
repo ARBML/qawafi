c71da12b5e44eea1fc353a033b75031d15ad035f
MagedSaeed <msmj2015@gmail.com>
2022-05-27 17:05:11 +0300

    add dockers add requiremenst add checkpoints

--- shakkelha_server/tests.py
+++ shakkelha_server/tests.py
@@ -15,7 +15,7 @@ from pprint import pprint
 for qaseeda in qasaed:
     print(qaseeda, type(qaseeda))
     response = requests.post(
-        "http://127.0.0.1:8000/api/diacritize",
+        "http://127.0.0.1:8080/api/diacritize",
         data={"baits": json.dumps(qaseeda, ensure_ascii=False)},
     )
     time.sleep(5)
