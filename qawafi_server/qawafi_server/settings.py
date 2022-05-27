e8f884e3d1e9114cfa642561e23aa39db30731a2
MagedSaeed <msmj2015@gmail.com>
2022-05-27 17:59:58 +0300

    restrucutre the project to be suitable for docker

--- qawafi_server/qawafi_server/settings.py
+++ qawafi_server/qawafi_server/settings.py
@@ -173,7 +173,7 @@ BOHOUR_NAMES_AR = [
 ]
 
 BOHOUR_PATTERNS = {}
-for bahr_file in glob.glob("../bohour_patterns/ints/*.txt"):
+for bahr_file in glob.glob("./bohour_patterns/ints/*.txt"):
     patterns = open(bahr_file, "r").read().splitlines()
     bahr_name = bahr_file.split("/")[-1].split(".")[0]
     if bahr_name not in BOHOUR_NAMES:
@@ -182,7 +182,7 @@ for bahr_file in glob.glob("../bohour_patterns/ints/*.txt"):
     BOHOUR_PATTERNS[bahr_name] = patterns
 
 BOHOUR_TAFEELAT = {}
-for bahr_file in glob.glob("../bohour_patterns/strs/*.txt"):
+for bahr_file in glob.glob("./bohour_patterns/strs/*.txt"):
     patterns = open(bahr_file, "r").read().splitlines()
     bahr_name = bahr_file.split("/")[-1].split(".")[0]
     if bahr_name not in BOHOUR_NAMES:
@@ -196,11 +196,11 @@ from .meters import create_transformer_model, create_model_v1, create_era_theme_
 
 
 METERS_MODEL = create_transformer_model()
-METERS_MODEL.load_weights("../meters_model/cp.ckpt")
+METERS_MODEL.load_weights("./deep-learning-models/meters_model/cp.ckpt")
 
 # get the embeddings model
 BASE_MODEL = create_model_v1()
-BASE_MODEL.load_weights("../embeddings_extractor_model/cp.ckpt")
+BASE_MODEL.load_weights("./deep-learning-models/embeddings_extractor_model/cp.ckpt")
 
 from tensorflow.keras.models import Model
 
@@ -208,20 +208,20 @@ FEATURES_EXTRACTOR = Model(BASE_MODEL.layers[0].input, BASE_MODEL.layers[4].outp
 
 from datasets import load_from_disk
 
-BAITS_EMBEDDINGS = load_from_disk("../baits_embeddings")
+BAITS_EMBEDDINGS = load_from_disk("./deep-learning-models/baits_embeddings")
 
 
 import tkseem as tk
 
 ERA_MODEL = create_era_theme_model()
-ERA_MODEL.load_weights("../era_classification_model/cp.ckpt")
+ERA_MODEL.load_weights("./deep-learning-models/era_classification_model/cp.ckpt")
 
 ERA_TOKENIZER = tk.SentencePieceTokenizer()
-ERA_TOKENIZER.load_model("../era_classification_model/vocab.model")
+ERA_TOKENIZER.load_model("./deep-learning-models/era_classification_model/vocab.model")
 
 
 THEME_MODEL = create_era_theme_model()
-THEME_MODEL.load_weights("../theme_classification_model/cp.ckpt")
+THEME_MODEL.load_weights("./deep-learning-models/theme_classification_model/cp.ckpt")
 
 THEME_TOKENIZER = tk.SentencePieceTokenizer()
-THEME_TOKENIZER.load_model("../theme_classification_model/vocab.model")
+THEME_TOKENIZER.load_model("./deep-learning-models/theme_classification_model/vocab.model")
