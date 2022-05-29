976a148f90771bed0c7f878649648dbe84b494d0
Omar <hammado92@gmail.com>
2022-05-29 16:33:13 -0600

    fixed-bar

--- Bohour_iOS/Bohour_iOS/ContentView.swift
+++ Bohour_iOS/Bohour_iOS/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
     @FocusState private var nameIsFocused: Bool
     
     var body: some View {
-        VStack{
+        ZStack(alignment:.top){
             HStack{
                 Image(systemName: "info.circle")
                     .font(.system(size: 20))
@@ -42,175 +42,179 @@ struct ContentView: View {
                         aboutShown = true
                     }
             }
-            if !errorMessage.isEmpty {
-                Text(errorMessage)
-                    .font(.system(size: 12))
-                    .foregroundColor(.red)
-                    .frame(maxWidth:.infinity)
-                    .padding(12)
-                    .background(Color.red.opacity(0.2))
-                    .cornerRadius(8)
-                    .padding(.horizontal)
+            VStack{
+                if !errorMessage.isEmpty {
+                    Text(errorMessage)
+                        .font(.system(size: 12))
+                        .foregroundColor(.red)
+                        .frame(maxWidth:.infinity)
+                        .padding(12)
+                        .background(Color.red.opacity(0.2))
+                        .cornerRadius(8)
+                        .padding(.horizontal)
 
-            }
-            VStack(alignment:.leading){
-                Text("اكتب قصيدة لتحليلها")
-                    .font(.system(size: 32, weight: .bold))
-                    .padding(.bottom,4)
-                Text("اكتب نص القصيدة وافصل كل شطر في سطر جديد")
-                    .foregroundColor(Color.gray_6)
-                    .padding(.bottom,12)
-                ZStack(alignment:.bottomTrailing){
-                    //editor
-                    TextEditor(text: $firstPart)
-                        .frame(height:240)
-                        .font(.system(size: 24))
-                        .modifier(TextFieldModifier())
-                        .foregroundColor(firstPart == placeholderText ? Color.gray_3 : Color.myDark)
-                        .lineSpacing(8)
-                        .focused($nameIsFocused)
-                        .onTapGesture {
-                            if firstPart == placeholderText {
-                                firstPart = ""
-                            }
-                        }
-                    //counter
-                    HStack{
-                        Button {
-                            let pb: UIPasteboard = UIPasteboard.general
-                            firstPart = pb.string ?? ""
-                        } label: {
-                            Label("لصق", systemImage: "doc.on.clipboard")
-                                .foregroundColor(.myPrimary)
-                                .padding(.horizontal,8)
-                        }
-                        
-                        Button {
-                            firstPart = ""
-                        } label: {
-                            Label("مسح", systemImage: "trash")
-                                .foregroundColor(firstPart.count == 0 || firstPart == placeholderText ? .white : .red.opacity(0.7))
-                                .padding(.horizontal,8)
-                        }
-
-                        Spacer()
-                        Text("عدد الأبيات \(numOfBayts)")
-                            .foregroundColor(numOfBayts == 0 ? Color.gray_3 : Color.myDark)
-                        
-                    }
-                    .padding()
                 }
-                Spacer()
-                Text("جاري تحليل الأبيات ...")
-                    .opacity(isloading ? 1 : 0)
-                    .foregroundColor(.myPrimary)
-                    .frame(maxWidth:.infinity)
-                Button {
-                    
-                    if !isButtonEnabled() {
-                        return
-                    }
-                    
-                    //1. check form
-                    if firstPart.count < 10  {
-                        withAnimation {
-                            errorMessage = "الرجاء التأكد من كتابة بيتي شعر"
+                VStack(alignment:.leading){
+                    Text("اكتب قصيدة لتحليلها")
+                        .font(.system(size: 32, weight: .bold))
+                        .padding(.bottom,4)
+                    Text("اكتب نص القصيدة وافصل كل شطر في سطر جديد")
+                        .foregroundColor(Color.gray_6)
+                        .padding(.bottom,12)
+                    ZStack(alignment:.bottomTrailing){
+                        //editor
+                        TextEditor(text: $firstPart)
+                            .frame(height:240)
+                            .font(.system(size: 24))
+                            .modifier(TextFieldModifier())
+                            .foregroundColor(firstPart == placeholderText ? Color.gray_3 : Color.myDark)
+                            .lineSpacing(8)
+                            .focused($nameIsFocused)
+                            .onTapGesture {
+                                if firstPart == placeholderText {
+                                    firstPart = ""
+                                }
+                            }
+                        //counter
+                        HStack{
+                            Button {
+                                let pb: UIPasteboard = UIPasteboard.general
+                                firstPart = pb.string ?? ""
+                            } label: {
+                                Label("لصق", systemImage: "doc.on.clipboard")
+                                    .foregroundColor(.myPrimary)
+                                    .padding(.horizontal,8)
+                            }
+                            
+                            Button {
+                                firstPart = ""
+                            } label: {
+                                Label("مسح", systemImage: "trash")
+                                    .foregroundColor(firstPart.count == 0 || firstPart == placeholderText ? .white : .red.opacity(0.7))
+                                    .padding(.horizontal,8)
+                            }
+
+                            Spacer()
+                            Text("عدد الأبيات \(numOfBayts)")
+                                .foregroundColor(numOfBayts == 0 ? Color.gray_3 : Color.myDark)
+                            
                         }
-                        return
+                        .padding()
                     }
-                    
-                    //2. no errors
-                    errorMessage = ""
-                    
-                    
-                    //3. start loading
-                    isloading = true
-                    disableButton = true
-                    nameIsFocused = false
-                    
-                    
-                    //4. call api
-                    Task{
-                        do{
-                            if let response = try? await API.getAnalysis(firstPart) {
-                                
-                                //5. show results
-                                Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { timer in
+                    Spacer()
+                    Text("جاري تحليل الأبيات ...")
+                        .opacity(isloading ? 1 : 0)
+                        .foregroundColor(.myPrimary)
+                        .frame(maxWidth:.infinity)
+                    Button {
+                        
+                        if !isButtonEnabled() {
+                            return
+                        }
+                        
+                        //1. check form
+                        if firstPart.count < 10  {
+                            withAnimation {
+                                errorMessage = "الرجاء التأكد من كتابة بيتي شعر"
+                            }
+                            return
+                        }
+                        
+                        //2. no errors
+                        errorMessage = ""
+                        
+                        
+                        //3. start loading
+                        isloading = true
+                        disableButton = true
+                        nameIsFocused = false
+                        
+                        
+                        //4. call api
+                        Task{
+                            do{
+                                if let response = try? await API.getAnalysis(firstPart) {
+                                    
+                                    //5. show results
+                                    Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { timer in
+                                        isloading = false
+                                        disableButton = false
+                                        self.response = response
+                                        
+                                    }
+                                    
+
+                                }else{
+                                    withAnimation {
+                                        errorMessage = "الرجاء التأكد من كتابة أبيات صحيحة"
+                                    }
                                     isloading = false
                                     disableButton = false
-                                    self.response = response
+                                    nameIsFocused = true
+                                    Timer.scheduledTimer(withTimeInterval:3, repeats: false) { timer in
+                                        withAnimation {
+                                            errorMessage = ""
+                                        }
+                                    }
                                     
                                 }
-                                
-
-                            }else{
-                                withAnimation {
-                                    errorMessage = "الرجاء التأكد من كتابة أبيات صحيحة"
-                                }
-                                isloading = false
-                                disableButton = false
-                                nameIsFocused = true
-                                Timer.scheduledTimer(withTimeInterval:3, repeats: false) { timer in
-                                    withAnimation {
-                                        errorMessage = ""
-                                    }
-                                }
-                                
                             }
                         }
+                        
+                    } label: {
+                        Group{
+                            if isloading {
+                                LoadingView()
+                            }else{
+                                Text("تحليل القصيدة").bold()
+                            }
+                        }
+                        .disabled(!isButtonEnabled())
+                        .modifier(ButtonModifier())
+                        .opacity(isButtonEnabled() ? 1 : 0.5 )
                     }
-                    
-                } label: {
-                    Group{
-                        if isloading {
-                            LoadingView()
-                        }else{
-                            Text("تحليل القصيدة").bold()
+                }
+                .padding(.horizontal, 32)
+            }
+            .padding(.top,72)
+            .sheet(item: $response) {
+            } content: { response in
+                ResultsView(response: response)
+                    .environment(\.layoutDirection, .rightToLeft)
+                    .environment(\.locale,.init(identifier: "ar"))
+                    .preferredColorScheme(.light)
+            }
+            .sheet(isPresented: $aboutShown, content: {
+                AboutView()
+                    .environment(\.layoutDirection, .rightToLeft)
+                    .environment(\.locale,.init(identifier: "ar"))
+                    .preferredColorScheme(.light)
+            })
+            .sheet(isPresented: $introViewShown, onDismiss: {
+                
+            }, content: {
+                IntroView()
+                    .environment(\.layoutDirection, .rightToLeft)
+                    .environment(\.locale,.init(identifier: "ar"))
+                    .preferredColorScheme(.light)
+            })
+            .onAppear {
+                
+                //first launch
+                if !launchedBefore  {
+                    //show first one
+                    Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false) { timer in
+                        withAnimation(Animation.spring()) {
+                            introViewShown = true
                         }
                     }
-                    .disabled(!isButtonEnabled())
-                    .modifier(ButtonModifier())
-                    .opacity(isButtonEnabled() ? 1 : 0.5 )
+                    UserDefaults.standard.set(true, forKey: "launchedBefore")
                 }
             }
-            .padding(32)
+            .onChange(of: firstPart) { newValue in
+                numOfBayts = firstPart.split(separator: "\n").count / 2
+                firstPart = firstPart.replacingOccurrences(of: "\n\n", with: "\n")
         }
-        .sheet(item: $response) {
-        } content: { response in
-            ResultsView(response: response)
-                .environment(\.layoutDirection, .rightToLeft)
-                .environment(\.locale,.init(identifier: "ar"))
-                .preferredColorScheme(.light)
-        }
-        .sheet(isPresented: $aboutShown, content: {
-            AboutView()
-                .environment(\.layoutDirection, .rightToLeft)
-                .environment(\.locale,.init(identifier: "ar"))
-                .preferredColorScheme(.light)
-        })
-        .sheet(isPresented: $introViewShown, onDismiss: {
-            
-        }, content: {
-            IntroView()
-                .environment(\.layoutDirection, .rightToLeft)
-                .environment(\.locale,.init(identifier: "ar"))
-                .preferredColorScheme(.light)
-        })
-        .onAppear {
-            
-            //first launch
-            if !launchedBefore  {
-                //show first one
-                Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false) { timer in
-                    withAnimation(Animation.spring()) {
-                        introViewShown = true
-                    }
-                }
-                UserDefaults.standard.set(true, forKey: "launchedBefore")
-            }
-        }
-        .onChange(of: firstPart) { newValue in
-            numOfBayts = firstPart.split(separator: "\n").count / 2
         }
         
     }
