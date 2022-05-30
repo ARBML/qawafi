69def26a7a65d32a542ab217aaa0faf3678a488b
MagedSaeed <msmj2015@gmail.com>
2022-05-31 00:24:49 +0300

    add override_tashkeel, clean input

--- qawafi_server/qawafi_api/views.py
+++ qawafi_server/qawafi_api/views.py
@@ -13,6 +13,8 @@ from difflib import SequenceMatcher
 from django.conf import settings
 from pyarabic.araby import strip_tashkeel
 
+from qawafi_server.utils import clean
+
 # Create your views here.
 
 majority_vote = lambda a: Counter(a).most_common()[0][0]
@@ -33,7 +35,8 @@ class BaitAnalyzerAPIView(View):
         lines = input.strip().split("\n")
         baits = []
         for i in range(len(lines) // 2):
-            bait = " # ".join(lines[i * 2 : (i + 1) * 2])
+            bait = "#".join(lines[i * 2 : (i + 1) * 2])
+            bait = clean(bait)
             baits.append(bait)
         return baits
 
@@ -42,7 +45,13 @@ class BaitAnalyzerAPIView(View):
         analyzer = settings.BAITS_ANALYZER
         # baits = [baits[0]]
         diacritized_baits = self.diacritize(baits)["diacritized"]
-        analysis = analyzer.analyze(baits=baits,diacritized_baits=diacritized_baits,return_closest_baits=False,short_qafiyah=True)
+        analysis = analyzer.analyze(
+            baits=baits,
+            diacritized_baits=diacritized_baits,
+            return_closest_baits=False,
+            short_qafiyah=True,
+            override_tashkeel=True,
+        )
         return JsonResponse(
             analysis,
             json_dumps_params={"ensure_ascii": False},
